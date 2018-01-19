7d27ef9ab7921a9fde0c1fb6a933ca059ebc3bc5
vertigoner <nbroberts97@gmail.com>
2018-01-19 14:27:01 -0500

    track object

---
+++ wrek-scrob/track.py
@@ -0,0 +1,35 @@
+
+# Simple track object
+class track():
+
+
+	def __init__(self, artist = None, album = None, title = None):
+
+		self.artist = artist
+		self.album = album
+		self.title = title
+
+
+	def getArtist():
+
+		return artist
+
+	def getAlbum():
+
+		return album
+
+	def getTitle():
+
+		return title
+
+	def setArtist(artist):
+
+		self.artist = artist
+
+	def setAlbum(album):
+
+		self.album = album
+
+	def setTitle(title):
+
+		self.title = title
