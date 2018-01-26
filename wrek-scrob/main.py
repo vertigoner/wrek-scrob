888f3bdc2f6dfa2eed7defb84385358dbb3482b8
vertigoner <nbroberts97@gmail.com>
2018-01-26 13:08:19 -0500

    improvements!

--- wrek-scrob/main.py
+++ wrek-scrob/main.py
@@ -1,8 +1,14 @@
 import sys
 import os
+import schedule
 import time
 
 sys.path.append(os.path.abspath('./../config'))
+
+from colorama import init
+init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
+from termcolor import cprint 
+from pyfiglet import figlet_format
 
 from env import *
 from lastfm import *
@@ -10,48 +16,56 @@ from scraper import *
 from track import *
 
 
-def scrapeAndScrob(lfm, currTrack):
+def initialize():
+	global currTrack
+
+	cprint(figlet_format('wrek scrob', font='starwars'),
+       'yellow', 'on_red', attrs=['bold'])
 	newTrack = scrape()
 
-	# base case
-	if currTrack is None:
+	if newTrack.validateTime():
 		lfm.updateNowPlaying(newTrack)
-		print 'Updated now playing: ' + newTrack.getArtist() + ' - ' + newTrack.getTitle()
-		return newTrack
+		print 'init: Updated now playing: ' + newTrack.getArtist() + ' - ' + newTrack.getTitle()
+		currTrack = newTrack
+	else:
+		print 'init: No current track scraped.'
+
+
+def job():
+	global currTrack
+	newTrack = scrape()
 
 	# if track is old (invalid) -> scrobble and stop updating now playing
-	elif not currTrack.validateTime():
+	if not newTrack.validateTime():
 		if not currTrack.hasBeenScrobbled():
 			lfm.scrobble(currTrack)
 			print 'Scrobbled track: ' + currTrack.getArtist() + ' - ' + currTrack.getTitle()
 			currTrack.setScrobbled(True)
-		print 'No current track scrobbled.'
-		return currTrack
+		print 'No current track scraped.'
 
 	# new track scraped -> scrobble old track and update now playing
 	elif not newTrack == currTrack:
-		lfm.scrobble(currTrack) # don't need to update currTrack.scrobbled since instance stops here
+		if currTrack.validateTime():
+			lfm.scrobble(currTrack) # don't need to update currTrack.scrobbled since instance stops here
 		lfm.updateNowPlaying(newTrack)
 		print 'Scrobbled track: ' + currTrack.getArtist() + ' - ' + currTrack.getTitle()
 		print 'Updated now playing: ' + newTrack.getArtist() + ' - ' + newTrack.getTitle()
-		return newTrack
 
-	# same track as old track (and valid) -> update now playing
+	# same track as old (valid) track -> do nothing
 	else:
-		lfm.updateNowPlaying(currTrack)
-		print 'Updated now playing: ' + newTrack.getArtist() + ' - ' + newTrack.getTitle()
-		return currTrack
+		return
 
 
 if __name__ == '__main__':
    
     lfm = lastfm()
     currTrack = None
-    currValid = False
 
-    print 'Starting wrek scrobbler...'
+    initialize()
+    schedule.every(30).seconds.do(job)
 
+    # TODO: catch exceptions
     while True:
-    	currTrack = scrapeAndScrob(lfm, currTrack)
-    	time.sleep(30)
+    	schedule.run_pending()
+    	time.sleep(1)
 
